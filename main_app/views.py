077917243611a0c0cdfdfa1c3944c1ecfaaf724d
21stPhenom <phenom.h.acks21@gmail.com>
2023-03-23 11:41:01 +0100

    removed unused object

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from django.utils.datastructures import MultiValueDictKeyError
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django.views import View
